06a616733ecb551adbd2d2818032d3cdb2dd8183
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-17 04:25:36 +0000

    Update Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -1,43 +1,86 @@
 /* eslint-disable @next/next/no-img-element */
 const data = [
 	{
-		title: "Music",
+		title: "Indie Game",
 		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		icon: "/music.png",
-		blobColour: "bg-purple-400",
+		blobColour: "bg-yellow-600",
+		icon: "/indie.gif",
+		alignInverse: true,
+		link: "https://github.com/kangah-codes/indie-game-concept",
 	},
 	{
-		title: "Coding",
+		title: "Campusride",
 		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		icon: "/keyboard.png",
+		blobColour: "bg-red-700",
+		icon: "/campusride.png",
+		link: "https://campusridegh.herokuapp.com/",
+	},
+	{
+		title: "Brick Breaker",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		icon: "/brick-game.png",
+		blobColour: "bg-purple-700",
+		link: "https://github.com/kangah-codes/brick_breaker",
+	},
+	{
+		title: "Metas Strip",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		icon: "/metas.png",
 		blobColour: "bg-baseColour",
+		link: "https://github.com/kangah-codes/metas-strip",
 	},
 	{
-		title: "Gaming",
+		title: "Flasker",
 		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		icon: "/gaming.png",
-		blobColour: "bg-white",
+		blobColour: "bg-pink-700",
+		link: "https://github.com/kangah-codes/flasker",
+	},
+
+	{
+		title: "Fingerprint attendance system",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		blobColour: "bg-green-700",
+		link: "https://github.com/kangah-codes/fingerprint-attendance-system",
 	},
 ];
 
 const Card = ({ data }) => {
-	const { title, text, icon, blobColour } = data;
+	const { title, text, icon, blobColour, alignInverse, link } = data;
 	return (
 		<div
 			className="bg-borderColour filter backdrop-blur-lg z-50 opacity-50 hover:opacity-100 focus:opacity-100
-                rounded-2xl space-y-5 p-2 relative flex flex-col hover:border-baseColour transition duration-500"
+                rounded-2xl space-y-5 p-2 relative flex flex-col transition duration-500"
 		>
-			<div className="w-full rounded-2xl bg-baseColour h-72 relative overflow-hidden">
-				<img
-					src="/test.png"
-					alt="project"
-					className="absolute left-10 top-10"
-				/>
-			</div>
+			{icon ? (
+				<div
+					className={`w-full rounded-2xl ${blobColour} h-72 relative overflow-hidden`}
+				>
+					<img
+						src={icon}
+						alt="project"
+						className={`absolute ${
+							alignInverse
+								? "right-12 bottom-12"
+								: "left-12 top-12"
+						} rounded-lg`}
+					/>
+				</div>
+			) : (
+				<div className={`w-full rounded-2xl ${blobColour} h-72 flex`}>
+					<img
+						src="/confused.png"
+						alt="project"
+						className="m-auto rounded-lg"
+					/>
+				</div>
+			)}
 			<div className="p-4">
-				<p className="text-white md:text-3xl text-2xl font-semibold">
+				<a
+					className="text-white md:text-3xl text-2xl font-semibold hover:underline"
+					href={link}
+				>
 					{title}
-				</p>
+				</a>
 				<p className="text-white font-medium text-xl mt-4">{text}</p>
 			</div>
 		</div>
@@ -48,7 +91,7 @@ const Projects = () => {
 	return (
 		<div className="my-0 lg:my-20 max-w-7xl flex flex-col px-8 mx-auto items-start">
 			<p className="text-white md:text-5xl text-3xl font-semibold mb-4 cursive flex">
-				My projects
+				A few of my projects
 			</p>
 			<div className="w-full grid grid-cols-1 lg:grid-cols-3  z-50 relative  gap-5 ">
 				{data.map((card, key) => (
