9c86c1b3f416d3d9e46da8d5606b4ba74f332f2e
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-17 05:14:28 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Cards from "../components/Card";
-import Likes from "../components/Tech";
+import Likes from "../components/Interest";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
