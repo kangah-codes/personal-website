4be71ed75c20a348113207e3aa02849a8e5d4e23
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-17 00:40:51 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
+import Hero from "../components/Hero";
 
 export default function Home() {
 	return (
 		<Layout>
-			<div>
-				<p>Lmao</p>
-			</div>
+			<Hero />
 		</Layout>
 	);
 }
