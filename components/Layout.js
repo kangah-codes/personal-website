e6af2eba89476b343a669a3741b989c7f0f016d8
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-17 05:56:23 +0000

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,14 +4,25 @@ import NavBar from "./Nav";
 const Layout = ({
 	children,
 	title = "kangah.codes",
-	desc = `Insurerity Digital is a Software-as-a-service (SaaS) company that
-      provides tailored solutions to brokers and insurers seeking to fully
-      digitize their customer acquisition process.`,
+	desc = "Welcome to my website.",
 	pt = false,
 }) => {
 	return (
 		<div className="flex flex-col min-h-screen bg-black">
 			<Head>
+				<script
+					type="text/javascript"
+					dangerouslySetInnerHTML={{
+						__html: `
+								(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+								new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+								j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+								'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+								})(window,document,'script','dataLayer','GTM-WTDK4BZ');
+						`,
+					}}
+				/>
+
 				<title>{title}</title>
 				<meta name="description" content={desc} />
 				<meta property="og:type" content="website" />
@@ -23,24 +34,33 @@ const Layout = ({
 				/>
 				<meta
 					name="keywords"
-					content="Insurance, Online Insurance, Motor Insurance, Life Insurance, Digital Insurance, E-Insurance, Car Insurance"
+					content="Developer, Joshua, Joshua Akangah, kangah-codes"
 				/>
-				<meta name="author" content="Insurerity Digital" />
-				<meta property="og:site_name" content="Insurerity Digital" />
-				<meta property="og:url" content={`https://insurerity.com`} />
+				<meta name="author" content="Joshua Akangah" />
+				<meta property="og:site_name" content="kangah.codes" />
+				<meta property="og:url" content={`https://kangah.codes`} />
 				<meta name="twitter:card" content="summary" />
 				<meta name="twitter:title" content={title} />
 				<meta name="twitter:description" content={desc} />
 				<meta name="twitter:site" content="@propernounco" />
 				<meta name="twitter:creator" content="@propernounco" />
 				<link rel="icon" href="/favicon.ico" />
-				{/* <link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;900&display=swap"
-					rel="stylesheet"
-				/> */}
-				{/* <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap" rel="stylesheet"/> */}
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-GP0K3G6G40"
+				></script>
+				<script
+					type="text/javascript"
+					dangerouslySetInnerHTML={{
+						__html: `
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+
+								gtag('config', 'G-GP0K3G6G40');
+						`,
+					}}
+				/>
 			</Head>
 
 			{/* <div className="z-50">
