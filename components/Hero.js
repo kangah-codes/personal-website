eba92fd05f79a893c6d925b2943faa6870b7f2cf
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-17 02:37:05 +0000

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 const Hero = () => {
 	return (
-		<div className="m-auto max-w-7xl py-20 pt-20 md:pt-48 xl:pt-36 px-8 text-white">
+		<div className="m-auto max-w-7xl py-20 pt-48 xl:pt-36 px-8 text-white">
 			<div className="grid grid-cols-1 lg:grid-cols-2 w-full items-start">
 				<div className="md:text-5xl text-2xl my-auto">
 					<h1 className="tracking-tight font-bold text-white leading-tighter mb-4 leading-normal">
@@ -35,16 +35,15 @@ const Hero = () => {
 					</div>
 				</div>
 
-				<div className="w-full flex relative lg:items-center lg:justify-center mt-20 h-96">
+				<div className="w-full flex relative items-center justify-center mt-20 h-96">
 					<div className="absolute top-0 left-5 w-72 h-72 bg-purple-900 rounded-full filter blur-3xl animate-blob" />
 					<div className="absolute top-0 -right-4 w-72 h-72 bg-baseColour rounded-full filter blur-3xl animate-blob animation-delay-2000" />
 					<div className="absolute top-28 right-36 w-72 h-72 bg-orange-600 rounded-full filter blur-3xl animate-blob animation-delay-4000" />
-					<div className="space-y-4 absolute" style={{ left: "50%" }}>
+					<div className="space-y-4 absolute">
 						<img
 							src="/my-photo.png"
 							alt="me"
-							className="relative"
-							style={{ left: "-50%" }}
+							className="relative w-96"
 						/>
 
 						{/* <div className="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
