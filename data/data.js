051b45494c2d28091d3a68ff76d437177fe4ace8
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-18 23:55:02 +0000

    Update data.js

--- data/data.js
+++ data/data.js
@@ -126,6 +126,14 @@ const tech = [
 	"arduino.png",
 ];
 
+const softSkills = [
+	"critical thinking",
+	"problem solving",
+	"public speaking",
+	"teamwork",
+	"leadership",
+];
+
 export const Technologies = () => (
 	<div>
 		<p>Technologies I work with</p>
@@ -138,6 +146,176 @@ export const Technologies = () => (
 					alt={item}
 					className="rounded-md w-10"
 				/>
+			))}
+		</div>
+	</div>
+);
+
+const workExperience = [
+	{
+		name: "Insurerity Digital",
+		website: "https://insurerity.com",
+		work: [
+			{
+				duration: "Feb 2021 - Present",
+				description: [
+					"Built beautiful client portals which facilitated clients to purchase & verify insurance policies, file claims and access detailed records & reports of their insurance portfolio",
+					"Redesigned and developed the new company website",
+				],
+				position: "Full Stack Developer, Part time",
+			},
+			{
+				duration: "Oct 2020 - Feb 2021",
+				description: [
+					"Wrote and tested APIs which powers several Insurance brokerage & aggregation software",
+					"Developed algorithms to handle complex policy premium calculations to conform to the NICs standard",
+					"Built APIs to automate the process of sending motor policy information to the NICMID",
+					"Worked devops using Docker, GCP, Heroku and Git/Github",
+				],
+				position: "Backend Developer, Part time",
+			},
+		],
+		icon: "insurerity.jpg",
+	},
+	{
+		name: "Freelance",
+		work: [
+			{
+				duration: "Nov 2019 - Present",
+				description: [
+					"I build lightweight, fast, simple but performant full stack web applications and APIs for small scale businesses, schools and individuals.",
+				],
+				position: "Full Stack Developer, Part time",
+			},
+		],
+	},
+	{
+		name: "ELiTE Education",
+		website: "https://elite-education.org",
+		work: [
+			{
+				duration: "July - October 2020",
+				description: [
+					"Developed a fully functional MVP for a LMS using ReactJs",
+					"Worked alongside colleagues using the K-12 architecture to develop an API that enables the LMS to interact with Salesforce",
+				],
+				position: "Full Stack Developer, Internship",
+			},
+		],
+		icon: "elite.png",
+	},
+	{
+		name: "IGNIS Technologies",
+		work: [
+			{
+				duration: "February - July 2020",
+				description: [
+					"Implemented logic and algorithms for a cashless transaction system for shuttle rides using Python, Flask and PostgreSQL",
+					"Developed client & admin dashboards using HTML, CSS and Bootstrap",
+					"Worked devops using tools like CircleCI, Heroku and Git/Github",
+				],
+				position: "Software Engineering Intern",
+			},
+		],
+	},
+	{
+		name: "Afiewura Housing Ltd",
+		website: "https://afiewura.com",
+		work: [
+			{
+				duration: "December 2019 - June 2020",
+				description: ["Developed and maintained the company's website"],
+				position: "Full Stack Developer, Part time",
+			},
+		],
+		icon: "afiewura.png",
+	},
+	{
+		name: "ELiTE Education",
+		website: "https://elite-education.org",
+		work: [
+			{
+				duration: "July - August 2019",
+				description: [
+					"Developed a curriculum to introduce students to concepts in STEM education, Web App Development and embedded systems using mobile electronic science labs which simplify otherwise complicated lab experiments.",
+					"Developed teaching content for a full course in developing web applications with Python + Flask and Google App Engine",
+				],
+				position: "STEM R&D Intern",
+			},
+		],
+		icon: "elite.png",
+	},
+];
+
+export const WorkExperience = () => (
+	<div>
+		<p className="mb-2 underline">Work Experience</p>
+
+		<div className="flex flex-col space-y-4">
+			{workExperience.map((item, key) => (
+				<div className="flex flex-col" key={key}>
+					<div className="flex">
+						<img
+							src={
+								item.icon
+									? `/work_experience/${item.icon}`
+									: "/not-found.png"
+							}
+							className="w-10 h-10 my-auto rounded-md no-mt"
+							alt="elite"
+						/>
+						<a
+							href={item.website ? item.website : "#"}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="my-auto ml-2 big-text hover:underline"
+						>
+							{item.name}
+						</a>
+					</div>
+					<div className="flex flex-col space-y-2">
+						{item.work.map((work, key) => (
+							<div key={key}>
+								<div className="flex space-x-2">
+									<p>{work.position},</p>
+									<p>{work.duration}</p>
+								</div>
+								<ul className="l-disc">
+									{work.description.map((desc, key) => (
+										<li className="ml-4 " key={key}>
+											{desc}
+										</li>
+									))}
+								</ul>
+							</div>
+						))}
+					</div>
+				</div>
+			))}
+		</div>
+		<style jsx global>{`
+			.big-text {
+				font-size: 1.25rem !important;
+				margin-top: auto !important;
+				margin-bottom: auto !important;
+			}
+			.no-mt {
+				margin-top: 0 !important;
+			}
+			.l-disc {
+				list-style-type: disc !important;
+			}
+		`}</style>
+	</div>
+);
+
+export const SoftSkills = () => (
+	<div>
+		<p>My soft skills include</p>
+
+		<div className="flex flex-col">
+			{softSkills.map((item, key) => (
+				<p key={key}>{item}</p>
 			))}
 		</div>
 	</div>
@@ -167,5 +345,15 @@ export const exampleFileSystem = {
 	unfinished_projects: {
 		type: "FOLDER",
 		children: { ...unfinished_projects },
+	},
+	soft_skills: {
+		type: "FILE",
+		content: <SoftSkills />,
+		extension: "txt",
+	},
+	work_experience: {
+		type: "FILE",
+		content: <WorkExperience />,
+		extension: "txt",
 	},
 };
