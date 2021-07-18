90689caeaa3f1a8b0b7e8d5fed4b77129794db71
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-18 17:37:16 +0000

    Update music.js

--- pages/music.js
+++ pages/music.js
@@ -1,12 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
 import Layout from "../components/Layout";
-import Hero from "../components/Hero";
-import Cards from "../components/Card";
-import Likes from "../components/Interest";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
 import Deck from "../components/Swipe";
 
 export default function Home() {
