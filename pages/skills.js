c0fa4e38c6f63c537dba348f204a46460906c39a
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-18 23:03:42 +0000

    Update skills.js

--- pages/skills.js
+++ pages/skills.js
@@ -11,28 +11,22 @@ export default function Home() {
 				>
 					My skills
 				</p>
-				<div className="max-w-md text-xl mt-4 text-white mx-auto text-center sm:mb-0 xl:mb-10">
+				<div className="max-w-md text-xl mt-4 text-white mx-auto text-center mb-8 lg:mb-20 xl:mb-10">
 					type help for a list of commands.
 				</div>
 				<div className="mx-auto w-full h-full">
-					<div className="relative h-full items-center justify-center flex">
-						{/* <div
-							className={`absolute top-1/3 left-1/4 w-64 h-64 bg-baseColour rounded-full filter blur-3xl animate-blob`}
-						/>
-						<div
-							className={`absolute top-1/3 left-1/4 w-64 h-64 bg-baseColour rounded-full filter blur-3xl animate-blob`}
-						/>
-						<div
-							className={`absolute top-1/3 left-1/4 w-64 h-64 bg-baseColour rounded-full filter blur-3xl animate-blob`}
-						/> */}
-
+					{/* <div className="relative h-full items-center justify-center flex">
 						<div className="absolute top-1/3 left-1/4 w-72 h-72 bg-green-500 rounded-full filter blur-3xl animate-blob" />
 						<div className="absolute top-1/3 left-1/5 w-72 h-72 bg-baseColour rounded-full filter blur-3xl animate-blob animation-delay-2000" />
 						<div className="absolute top-1/3 right-1/4 w-72 h-72 bg-orange-600 rounded-full filter blur-3xl animate-blob animation-delay-4000" />
 						<div className="absolute z-50">
 							<MyTerminal />
 						</div>
-					</div>
+					</div> */}
+					<div className="absolute top-1/2 left-1/2 w-28 xl:w-72 h-28 xl:h-72 bg-green-500 rounded-full filter blur-3xl animate-blob" />
+					<div className="absolute top-1/2 left-1/3 w-28 xl:w-72 h-28 xl:h-72 bg-baseColour rounded-full filter blur-3xl animate-blob animation-delay-2000" />
+					<div className="absolute top-1/2 right-1/4 w-28 xl:w-72 h-28 xl:h-72 bg-orange-600 rounded-full filter blur-3xl animate-blob animation-delay-4000" />
+					<MyTerminal />
 				</div>
 			</div>
 			<div className="mb-20 md:mb-0" />
