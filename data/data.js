3e74bb464f6244abacf7699fb7a6ac1954e9d688
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-18 15:21:28 +0000

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,45 @@
+export const thingsIdo = [
+	{
+		title: "Frontend Development",
+		text: `I build attractive and responsive web apps (like the one you're viewing) using tools such as Reactjs, Nextjs, and good old-fashioned HTML/CSS.`,
+		icon: "/laptop.png",
+		blobColour: "bg-purple-400",
+	},
+	{
+		title: "Backend Development",
+		text: `I have experience in building APIs and microservices using a wide array of frameworks like Express, Django, FastAPI, Flask, etc. to deliver blazing fast services.`,
+		icon: "/keyboard.png",
+		blobColour: "bg-baseColour",
+	},
+	{
+		title: "Devops",
+		text: `Devops is just that part of being a programmer you can't live without. Github actions? Check. CI/CD? Check. End-to-end testing? Check. Docker? CHECK.`,
+		icon: "/settings.png",
+		blobColour: "bg-white",
+	},
+];
+
+export const myInterests = [
+	{
+		title: "Music",
+		text: `
+			I love listening to music. I love Michael Jackson 👑, The Beatles, Queen, Led Zeppelin, Lynyrd Skynyrd, and many many more artists.
+		`,
+		icon: "/music.png",
+		blobColour: "bg-purple-400",
+	},
+	{
+		title: "Coding",
+		text: `
+			I love programming. Programming has taught me how to approach problems and solve them in many ways. It's the perfect mental exercise for me.
+		`,
+		icon: "/keyboard.png",
+		blobColour: "bg-baseColour",
+	},
+	{
+		title: "Gaming",
+		text: `When I'm not making games, I'm playing them. I regularly play games like Red Dead Redemption 2, GTA V, FIFA 19, Minecraft and Mortal Kombat.`,
+		icon: "/gaming.png",
+		blobColour: "bg-white",
+	},
+];
