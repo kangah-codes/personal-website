b5c07d5a9574e48dd00ff372a71fb5eb7d42ed96
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-18 15:21:40 +0000

    Create skills.js

---
+++ pages/skills.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../components/Layout";
+import Hero from "../components/Hero";
+import Cards from "../components/Card";
+import Likes from "../components/Interest";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Deck from "../components/Swipe";
+
+export default function Home() {
+	return <Layout></Layout>;
+}
