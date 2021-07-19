818f1db51588dce62b4daef40f9f224e42296f3a
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-07-19 07:23:09 +0000

    Update skills.js

--- pages/skills.js
+++ pages/skills.js
@@ -3,8 +3,8 @@ import MyTerminal from "../components/Terminal";
 
 export default function Home() {
 	return (
-		<Layout>
-			<div className="w-full flex flex-col mx-auto max-w-7xl pb-10 pt-48 md:pt-36 px-8 h-screen">
+		<Layout hFull={false}>
+			<div className="w-full flex flex-col mx-auto max-w-7xl pb-10 pt-48 md:pt-36 px-4 md:px-8 h-screen">
 				<p
 					className="text-5xl text-center text-gradient animate-gradient-x bg-gradient-to-r from-baseColour
 					via-green-600 to-baseColour font-semibold"
@@ -14,7 +14,7 @@ export default function Home() {
 				<div className="max-w-md text-xl mt-4 text-white mx-auto text-center mb-8 lg:mb-20 xl:mb-10">
 					type help for a list of commands.
 				</div>
-				<div className="mx-auto w-full h-full">
+				<div className="mx-auto w-full pb-20">
 					{/* <div className="relative h-full items-center justify-center flex">
 						<div className="absolute top-1/3 left-1/4 w-72 h-72 bg-green-500 rounded-full filter blur-3xl animate-blob" />
 						<div className="absolute top-1/3 left-1/5 w-72 h-72 bg-baseColour rounded-full filter blur-3xl animate-blob animation-delay-2000" />
@@ -29,7 +29,6 @@ export default function Home() {
 					<MyTerminal />
 				</div>
 			</div>
-			<div className="mb-20 md:mb-0" />
 		</Layout>
 	);
 }
